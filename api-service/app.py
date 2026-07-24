5e208346093bb1e32d13aca8493573d9fa343bc0
fisch <fischsuppentopf@gmail.com>
2026-07-24 10:01:21 +0200

    api verschärft hinzugefügt

--- api-service/app.py
+++ api-service/app.py
@@ -7,9 +7,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 
